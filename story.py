641270bbc7ec1cbcdba3c4bc8f57bfd20f16578e
Paul Pedro <sablos@hotmail.com>
2020-04-19 17:49:34 -0400

    Added docstrings and cleaned up the presentation a bit

--- story.py
+++ story.py
@@ -1,6 +1,7 @@
 import textwrap
 
 file = "story.txt"
+title = "The Iron Sandwich"
 
 
 class Scene:
@@ -11,6 +12,7 @@ class Scene:
 
 
 def format_options(options):
+    """Converts the options line from the file into a list of dictionaries"""
     result = []
     opts = options.split("@")
     for opt in opts:
@@ -19,7 +21,12 @@ def format_options(options):
     return result
 
 
+def clr_screen():
+    """Clears the screen by inserting a number of linefeeds"""
+    print(50 * "\n")
+
 def display_all_scenes(story):
+    """Prints a list of all scenes, descriptions and options"""
     for scene in story:
         print("name =", scene.name)
         print("description")
@@ -30,6 +37,7 @@ def display_all_scenes(story):
 
 
 def get_scene(story, sc_name):
+    """Returns the scene from the story based on the scene name provided"""
     for scene in story:
         if scene.name == sc_name:
             return scene
@@ -37,13 +45,16 @@ def get_scene(story, sc_name):
 
 
 def display_scene(scene):
+    """Displays the description and options of the chosen scene"""
     print(textwrap.fill(scene.description, 70))
+    print("\n")
     for i, option in enumerate(scene.options):
         print(f"{i + 1}. {option['prompt']}")
 
 
 def choose_option(scene):
-    valid_choices = [str(x) for x in range(1, len(scene.options))]
+    """Allows the user to choose one of the options, and returns the scene name"""
+    valid_choices = [str(x) for x in range(1, len(scene.options) + 1)]
     choice = ""
     while choice not in valid_choices:
         choice = input("Choose what to do: ")
@@ -52,6 +63,7 @@ def choose_option(scene):
 
 
 def retrieve_scenes(file):
+    """Retrieves the story from the text file and returns it as a list of scenes"""
     scenes = []
     # read all the lines from the story into a list
     with open(file) as story:
@@ -61,7 +73,6 @@ def retrieve_scenes(file):
         sc_name = lines[3 * i].rstrip("\n")
         sc_desc = lines[3 * i + 1].rstrip("\n")
         options = lines[3 * i + 2].rstrip("\n")
-        # sc_opts = [opt.split(;) for opt in options]
         sc_opts = format_options(options)
         this_scene = Scene(sc_name, sc_desc, sc_opts)
         scenes.append(this_scene)
@@ -76,12 +87,14 @@ my_story = retrieve_scenes(file)
 end = False
 current_target = "scene1"
 
-print("Welcome to the story\n")
+print(f"* * *    {title}   * * * \n\n")
+input("Press enter to begin...")
 while not end:
     current_scene = get_scene(my_story, current_target)
     if current_scene is None:
         end = True
     else:
+        clr_screen()
         display_scene(current_scene)
         current_target = choose_option(current_scene)
 
